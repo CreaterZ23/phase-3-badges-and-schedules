3e654c56d597ab56cec920289ad362dc652ffaf5
Zeon <zeonstewart23@gmail.com>
2021-09-21 20:50:55 -0400

    commit tree 907f218f5468487049ce9202f5e1957de3e82508

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker name 
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_arr 
+    name_arr.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms name_arr
+    name_arr.map.with_index(1) do |speaker, room_num|
+        "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    end
+end
+
